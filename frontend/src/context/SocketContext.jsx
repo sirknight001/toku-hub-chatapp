255488408522f5ce76dbde34edd3e7205542fdb9
sirknight001 <christiantorres053@gmail.com>
2025-08-16 14:58:50 +0800

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -19,7 +19,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io('http://localhost:5000', {
+            const socket = io('https://toku-hub-chatapp.onrender.com/', {
                 query: {
                     userId: authUser._id
                 }
